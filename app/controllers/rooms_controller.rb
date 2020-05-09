4fab0e0401d1dd047fc3b32cc3dfe25aa5d56707
Shawn Huang Fernandes <shawn.huang.fernandes@gmail.com>
2020-05-09 15:20:33 -0700

    Heroku Merge

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -11,7 +11,7 @@ class RoomsController < ApplicationController
         @room = Room.new()
         if @room.save
             cellRange = (0..399).to_a
-            cellRange.each do |el| 
+            cellRange.each do |el|
                 Cell.create(index: el, room_id: @room.id)
             end
 
